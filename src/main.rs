326047f14cbd4219a2d0d800c979621a72a9d2b8
dalia <dalia.a.mirghani@gmail.com>
2026-03-11 20:48:01 +0200

    button attempt semi success

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
 use std::{io, path::Path};
-use ratatui::layout::Margin;
+use ratatui::{layout::Margin, style::Style};
 
 use crossterm::event::{KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{DefaultTerminal, Frame, layout::{Alignment, Constraint, Layout}, restore, style::Stylize, text::Line, widgets::{Block, Borders, Padding, Paragraph, Widget}};
 
 fn main()-> io::Result<()> {
     let mut terminal = ratatui::init(); //initiated terminal
-    let mut app = App {exit:false}; //initiated our application
-    let result= app.run(&mut terminal);
+    let mut app = App {main_menu:MainMenu { exit:false, button:Button {label:"Let's go".to_string(), is_pressed:false} }}; //initiated our application
+    let result= app.main_menu.run(&mut terminal);
     ratatui::restore(); 
     result
 }
 
 pub struct App{
-    exit:bool,
+    main_menu:MainMenu
 }
-impl  App {
+pub struct MainMenu{
+    exit:bool,
+    button:Button
+}
+pub struct Button{
+    label:String,
+    is_pressed:bool
+}
+impl Button{
+    fn if_pressed(){
+        //new scene logic whatever
+    }
+}
+impl Widget for &Button{
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+        where
+            Self: Sized {
+        let block= Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().bg(ratatui::style::Color::LightYellow).fg(ratatui::style::Color::Green));
+    block.render(area, buf);
+    }
+}
+impl MainMenu{
     fn run(&mut self,terminal:&mut DefaultTerminal)->io::Result<()>{
         while !self.exit{
             match crossterm::event::read()?{
@@ -35,8 +58,10 @@ impl  App {
         }
         Ok(())
     }
+
 }
-impl Widget for &App {
+
+impl Widget for &MainMenu {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized {
@@ -49,7 +74,7 @@ impl Widget for &App {
         let chunks =Layout::default()
         .direction(ratatui::layout::Direction::Vertical)
         // i made these constraints such that the text is centered
-        .constraints([Constraint::Min(0),Constraint::Length(10),Constraint::Length(10), Constraint::Min(0)])
+        .constraints([Constraint::Min(0),Constraint::Length(10),Constraint::Length(10),Constraint::Length(3), Constraint::Min(0)])
         .split(area);
 
         let name= Paragraph::new(r#" ██████  ███████  ██████     ██████  ██ ███████ ████████ ██████   ██████  
@@ -69,5 +94,9 @@ impl Widget for &App {
         "#).alignment(Alignment::Center)
         .bold()
         .render(chunks[2], buf);
+        
+        self.button.render(chunks[3], buf);
+            
+        
     }
 }
